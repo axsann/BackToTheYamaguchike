4433ee650f28d67826ba68132494c459e985e92e
kanta <santoorino3@yahoo.co.jp>
2015-02-01 10:57:13 +0900

    Background Scroll Down, Meiji People Walking, Meiji, Shouwa Bg Added.

--- BackToTheYamaguchike/GameScene.m
+++ BackToTheYamaguchike/GameScene.m
@@ -8,76 +8,122 @@
 
 #import "GameScene.h"
 #import "AppDelegate.h"
+#define BG_SCALE 0.93f
+#define BG_ORIGIN_WIDTH 930
+#define BG_ORIGIN_HIGHT 256
+#define INTERVAL 24.0f
 
 @implementation GameScene {
     AppDelegate *app;
-    CGFloat _additionW;
-    CGFloat _additionH;
     BOOL _isSettingMode;
-    SKSpriteNode *_edoBg;
-    NSMutableArray *_edoPeopleNodeArray;
+    NSMutableArray *_bgNodeArray;
+    NSMutableDictionary *_peopleNodeArrayDict;
     float _nextRunTime;
 }
 
 
 -(void)didMoveToView:(SKView *)view {
+    // Setup your scene here
     // AppDelegateをインスタンス化
     app = [[NSApplication sharedApplication] delegate];
-    _edoPeopleNodeArray = [NSMutableArray array];
-    // Setup your scene here
-    _additionW = 0.0f;
-    _additionH = 0.0f;
+    _peopleNodeArrayDict = [NSMutableDictionary dictionary];
+    _bgNodeArray = [NSMutableArray array];
     _nextRunTime = 0.0f;
     _isSettingMode = true;
     self.backgroundColor = [NSColor blackColor];
     self.view.window.backgroundColor = [NSColor grayColor];
-    [self addEdoBackground];
-    [self addPeople];
-    [self movePeopleOnce];
+    [self addAllEraBg];
+    [self addAllEraPeople];
+    [self moveAllEraPeopleOnce];
 }
 
 -(void)update:(CFTimeInterval)currentTime {
     // Called before each frame is rendered
-    [self movePeopleLoop];
-    //[self moveBackgroundIn30secInterval:currentTime];
+    [self moveAllEraPeopleLoop];
+    [self moveBgIn30secInterval:currentTime];
+    [self moveBgToFirst];
 }
 
--(void)addPeople {
-    [self addEdoPeople];
-    //[self addMeijiPeople];
+-(void)addAllEraBg {
+    [self addBgWithEraName:@"edo" atIndex:0];
+    [self addBgWithEraName:@"meiji" atIndex:1];
+    [self addBgWithEraName:@"shouwa" atIndex:2];
+    [self addBgWithEraName:@"heisei" atIndex:3];
 }
 
--(void)moveBackgroundIn30secInterval:(CFTimeInterval)currentTime {
-    float interval = 30.0f;
+-(void)addAllEraPeople {
+    [self addPeopleWithEraName:@"edo"];
+    [self addPeopleWithEraName:@"meiji"];
+}
+
+-(void)moveAllEraPeopleOnce {
+    [self movePeopleOnceWithEraName:@"edo"];
+    [self movePeopleOnceWithEraName:@"meiji"];
+}
+-(void)moveAllEraPeopleLoop {
+    [self movePeopleLoopWithEraName:@"edo"];
+    [self movePeopleLoopWithEraName:@"meiji"];
+}
+
+-(void)moveBgIn30secInterval:(CFTimeInterval)currentTime {
+    float interval = INTERVAL;
     if (_nextRunTime==0.0f) {
         _nextRunTime = currentTime+interval;
     }
     if (currentTime>=_nextRunTime) {
-        [self moveBackground];
+        [self moveBg];
         _nextRunTime = currentTime+interval;
     }
 }
 
-
--(void)moveBackground {
-    float positionY = _edoBg.position.y-_edoBg.size.height;
-    SKAction *moveUpAction = [SKAction moveToY:positionY duration:10];
-    [_edoBg runAction:moveUpAction];
-}
-
--(void)addEdoBackground {
+-(void)moveBg {
     CGSize screenSize = self.frame.size;
-    _edoBg = [SKSpriteNode spriteNodeWithImageNamed:@"edo_bg"];
-    CGSize bgSize = _edoBg.size;
-    _edoBg.position = CGPointMake(screenSize.width/2, screenSize.height-(bgSize.height/2));
-    [self addChild:_edoBg];
+    float distanceY = screenSize.height/3*-1;
+    for (SKSpriteNode *bg in _bgNodeArray) {
+        SKAction *moveDownAction = [SKAction moveByX:0 y:distanceY duration:10];
+        [bg runAction:moveDownAction];
+    }
 }
 
--(void)addEdoPeople {
-    //CGSize screenSize = self.frame.size;
-    CGSize bgSize = _edoBg.size;
-    NSDictionary *edoPeopleDict = app.peopleDict[@"edo"];
-    NSArray *peopleNameArray = [edoPeopleDict allKeys]; // 名前を取得
+-(void)moveBgToFirst {
+    CGSize screenSize = self.frame.size;
+    float positionY = screenSize.height+(screenSize.height/3)/2;
+    for (SKSpriteNode *bg in _bgNodeArray) {
+        if (bg.position.y <= -(screenSize.height/3)/2) {
+            bg.position = CGPointMake(bg.position.x, positionY);
+        }
+    }
+}
+
+-(void)addBgWithEraName:(NSString *)eraName atIndex:(NSInteger)index {
+    CGSize screenSize = self.frame.size;
+    NSString *bgName = [eraName stringByAppendingString:@"_bg"];
+    SKSpriteNode *bg = [SKSpriteNode spriteNodeWithImageNamed:bgName];
+    CGSize bgSize = bg.size;
+    bg.position = CGPointMake(screenSize.width/2, screenSize.height-(bgSize.height*index)-(bgSize.height/2));
+    bg.name = bgName;
+    bg.xScale = BG_SCALE;
+    bg.yScale = BG_SCALE;
+    [_bgNodeArray addObject:bg];
+    [self addChild:bg];
+}
+
+-(SKSpriteNode *)getBgWithEraName:(NSString *)eraName {
+    NSString *bgName = [eraName stringByAppendingString:@"_bg"];
+    for (SKSpriteNode *bg in _bgNodeArray) {
+        if ([bg.name isEqualToString:bgName]) {
+            return bg;
+        }
+    }
+    return nil;
+}
+
+ 
+-(void)addPeopleWithEraName:(NSString *)eraName {
+    SKSpriteNode *bg = [self getBgWithEraName:eraName];
+    NSDictionary *peopleDict = app.peopleDict[eraName]; // eraNameの時代のDictを読み込む
+    NSArray *peopleNameArray = [peopleDict allKeys]; // 名前を取得
+    NSMutableArray *peopleNodeArray = [NSMutableArray array];
     for (int i=0; i<peopleNameArray.count; i++) {
         NSString *peopleName = peopleNameArray[i]; // 配列からアトラスの名前を取得
         SKTextureAtlas *atlas = [SKTextureAtlas atlasNamed:peopleName]; // アトラスを取得
@@ -91,14 +137,12 @@
         // キャラクターを配置
         SKSpriteNode *people= [SKSpriteNode spriteNodeWithTexture:textureArray[0]];
         people.name = peopleName;
-        //people.xScale = 0.08;
-        //people.yScale = 0.08;
-        float positionX = (int)(arc4random() % (int)(bgSize.width)) + ((bgSize.width/2)*-1); // 位置を乱数にする
-        float positionY = (people.size.height/2 * -1)+(bgSize.height/5);
-        //people.position = CGPointMake(positionX, 600);
+        float positionX = (int)(arc4random() % (BG_ORIGIN_WIDTH)) + (int)(-(BG_ORIGIN_WIDTH/2)); // 位置を乱数にする
+        float positionY = (people.size.height/2 * -1)+(BG_ORIGIN_HIGHT/5);
         people.position = CGPointMake(positionX, positionY);
-        [_edoPeopleNodeArray addObject:people];
-        [_edoBg addChild:people];
+        [peopleNodeArray addObject:people];
+        [_peopleNodeArrayDict setObject:peopleNodeArray forKey:eraName];
+        [bg addChild:people];
         
         // アニメーションを実行
         SKAction *walkAction = [SKAction animateWithTextures:textureArray timePerFrame:0.4];
@@ -107,14 +151,14 @@
 }
 
 
--(void)movePeopleOnce {
-    NSDictionary *edoPeopleDict = app.peopleDict[@"edo"];
-    CGSize bgSize = _edoBg.size;
-    float leftEdgeX = bgSize.width/2 * -1;
-    for (SKSpriteNode *people in _edoPeopleNodeArray) {
+-(void)movePeopleOnceWithEraName:(NSString *)eraName {
+    NSDictionary *edoPeopleDict = app.peopleDict[eraName];
+    NSMutableArray *peopleNodeArray = _peopleNodeArrayDict[eraName];
+    float leftEdgeX = -(BG_ORIGIN_WIDTH/2.0f);
+    for (SKSpriteNode *people in peopleNodeArray) {
         NSString *peopleSpeedRateStr = edoPeopleDict[people.name]; // DictからSpeedRateを文字列型で取得
         float peopleSpeedRate = [peopleSpeedRateStr floatValue]; // SpeedRateを小数に変換
-        float peopleSpeed = bgSize.width / peopleSpeedRate; // スクリーンの幅をSpeedRateで割ったものをスピードにする
+        float peopleSpeed = BG_ORIGIN_WIDTH / peopleSpeedRate; // スクリーンの幅をSpeedRateで割ったものをスピードにする
         float distance = fabs(people.position.x - leftEdgeX);
         float moveDuration = distance / peopleSpeed;
         people.xScale = fabs(people.xScale);
@@ -124,15 +168,15 @@
     
 }
 
--(void)movePeopleLoop {
-    NSDictionary *edoPeopleDict = app.peopleDict[@"edo"];
-    CGSize bgSize = _edoBg.size;
-    float rightEdgeX = bgSize.width/2;
-    float leftEdgeX = bgSize.width/2 * -1;
-    for (SKSpriteNode *people in _edoPeopleNodeArray) {
+-(void)movePeopleLoopWithEraName:(NSString *)eraName {
+    NSDictionary *edoPeopleDict = app.peopleDict[eraName];
+    NSMutableArray *peopleNodeArray = _peopleNodeArrayDict[eraName];
+    float rightEdgeX = BG_ORIGIN_WIDTH/2.0f;
+    float leftEdgeX = -(BG_ORIGIN_WIDTH/2.0f);
+    for (SKSpriteNode *people in peopleNodeArray) {
         NSString *peopleSpeedRateStr = edoPeopleDict[people.name]; // DictからSpeedRateを文字列型で取得
         float peopleSpeedRate = [peopleSpeedRateStr floatValue]; // SpeedRateを小数に変換
-        float peopleSpeed = bgSize.width / peopleSpeedRate; // スクリーンの幅をSpeedRateで割ったものをスピードにする
+        float peopleSpeed = BG_ORIGIN_WIDTH / peopleSpeedRate; // スクリーンの幅をSpeedRateで割ったものをスピードにする
         if (people.position.x == rightEdgeX) {
             float distance = fabs(people.position.x - leftEdgeX);
             float moveDuration = distance / peopleSpeed;
@@ -150,54 +194,5 @@
     }
 
 }
-
-
-//========== For Setting ==========
--(void)keyDown:(NSEvent *)theEvent {
-    if (_isSettingMode){
-        if (theEvent.keyCode == 123) { // left arrow
-            _additionW += 1;
-        }else if (theEvent.keyCode == 124) { // right arrow
-            _additionW -= 1;
-        }else if (theEvent.keyCode == 125) { // down arrow
-            _additionH += 1;
-        }else if (theEvent.keyCode == 126) { // up arrow
-            _additionH -= 1;
-        }
-        self.view.frame = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y,
-                                 self.view.frame.size.width  - _additionW,
-                                 self.view.frame.size.height - _additionH);
-    }
-}
-
--(void)keyUp:(NSEvent *)theEvent {
- 
-    _additionW = 0.0f;
-    _additionH = 0.0f;
-    if (self.view.frame.size.width<100 || self.view.frame.size.height<100) {
-        self.view.frame = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y,
-                                     100.0f, 100.0f);
-    }
-    if (_isSettingMode) {
-        if (theEvent.keyCode == 1) { // 's'キー
-            _isSettingMode = false;
-        }
-    }else {
-        _isSettingMode = true;
-    }
-}
- 
- -(void)mouseDragged:(NSEvent *)theEvent {
-     if (_isSettingMode) {
-         CGPoint mouseLocation = [theEvent locationInWindow];
-         NSLog(@"mouseX: %f, mouseY: %f", mouseLocation.x, mouseLocation.y);
-         CGFloat viewWidthHalf = self.view.frame.size.width/2;
-         CGFloat viewHeightHalf = self.view.frame.size.height/2;
-         self.view.frame = CGRectMake(mouseLocation.x-viewWidthHalf, mouseLocation.y-viewHeightHalf,
-                                      self.view.frame.size.width, self.view.frame.size.height);
-     }
-}
-
-
 
 @end
